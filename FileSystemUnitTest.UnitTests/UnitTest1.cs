f9d256de82e6dd6797f2d0bf35f772f40b492aba
erbynn <john.erbynn@gmail.com>
2020-10-08 16:33:03 +0000

    .gitignore fix

--- FileSystemUnitTest.UnitTests/UnitTest1.cs
+++ FileSystemUnitTest.UnitTests/UnitTest1.cs
@@ -38,5 +38,40 @@ namespace FileSystemUnitTest.UnitTests
             _fileSystem.Verify(f => f.File.WriteAllText(It.IsAny<String>(), It.IsAny<String>()), Times.Once);
             _fileSystem.Verify(f => f.File.ReadAllText(It.IsAny<String>()), Times.Once);
         }
+
+        [Fact]
+        public void FileDealer_WhenFolderExists_FileDoesnNotExists_ReturnsContents()
+        {
+            _fileSystem.Setup(f => f.Directory.Exists(It.IsAny<String>())).Returns(true);
+            _fileSystem.Setup(f => f.File.Exists(It.IsAny<String>())).Returns(false);
+            var fileDealer = new FileDealer(_fileSystem.Object);
+            var result = fileDealer.FileProcessor();
+
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<String>(result);
+
+            _fileSystem.Verify(f => f.Directory.CreateDirectory(It.IsAny<String>()), Times.Never);
+            _fileSystem.Verify(f => f.File.Delete(It.IsAny<String>()), Times.Never);
+            _fileSystem.Verify(f => f.File.WriteAllText(It.IsAny<String>(), It.IsAny<String>()), Times.Once);
+            _fileSystem.Verify(f => f.File.ReadAllText(It.IsAny<String>()), Times.Once);
+        }
+
+        [Fact]
+        public void FileDealer_WhenFolderExists_FileExists_ReturnsContents()
+        {
+            _fileSystem.Setup(f => f.Directory.Exists(It.IsAny<String>())).Returns(true);
+            _fileSystem.Setup(f => f.File.Exists(It.IsAny<String>())).Returns(true);
+            var fileDealer = new FileDealer(_fileSystem.Object);
+            var result = fileDealer.FileProcessor();
+
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<String>(result);
+
+            _fileSystem.Verify(f => f.Directory.CreateDirectory(It.IsAny<String>()), Times.Never);
+            _fileSystem.Verify(f => f.File.Delete(It.IsAny<String>()), Times.Once);
+            _fileSystem.Verify(f => f.File.WriteAllText(It.IsAny<String>(), It.IsAny<String>()), Times.Once);
+            _fileSystem.Verify(f => f.File.ReadAllText(It.IsAny<String>()), Times.Once);
+        }
+
     }
 }
