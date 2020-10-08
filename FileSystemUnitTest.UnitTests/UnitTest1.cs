75bc72b910c8fdbc45a78996b53636cdf36363de
erbynn <john.erbynn@gmail.com>
2020-10-08 16:15:22 +0000

    first commit

---
+++ FileSystemUnitTest.UnitTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using AutoFixture;
+using Moq;
+using System;
+using System.IO.Abstractions;
+using Xunit;
+using FileSystemUnitTest;
+
+namespace FileSystemUnitTest.UnitTests
+{
+    public class FileDealerTest
+    {
+        public readonly Mock<IFileSystem> _fileSystem;
+        public readonly Fixture _fixture;
+
+        public FileDealerTest()
+        {
+            _fixture = new Fixture();
+            _fileSystem = new Mock<IFileSystem>();
+
+            _fileSystem.Setup(f => f.Directory.CreateDirectory(It.IsAny<String>())).Verifiable();
+            _fileSystem.Setup(f => f.File.Delete(It.IsAny<String>())).Verifiable();
+            _fileSystem.Setup(f => f.File.WriteAllText(It.IsAny<String>(), It.IsAny<String>())).Verifiable();
+            _fileSystem.Setup(f => f.File.ReadAllText(It.IsAny<String>())).Returns(_fixture.Create<String>());
+        }
+
+        [Fact]
+        public void FileDealer_WhenFolderDoesNotExists_ReturnsContents()
+        {
+            _fileSystem.Setup(f => f.Directory.Exists(It.IsAny<String>())).Returns(false);
+            var fileDealer = new FileDealer(_fileSystem.Object);
+            var result = fileDealer.FileProcessor();
+
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<String>(result);
+
+            _fileSystem.Verify(f => f.Directory.CreateDirectory(It.IsAny<String>()), Times.Once);
+            _fileSystem.Verify(f => f.File.Delete(It.IsAny<String>()), Times.Never);
+            _fileSystem.Verify(f => f.File.WriteAllText(It.IsAny<String>(), It.IsAny<String>()), Times.Once);
+            _fileSystem.Verify(f => f.File.ReadAllText(It.IsAny<String>()), Times.Once);
+        }
+    }
+}
---
+++ FileSystemUnitTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.IO.Abstractions;
+
+namespace FileSystemUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                FileDealer app = serviceProvider.GetService<FileDealer>();
+                app.FileProcessor();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.  
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FileDealer>();
+            services.AddScoped<IFileSystem, FileSystem>();
+        }
+    }
+
+    public class FileDealer
+    {
+        private readonly IFileSystem _fileSytem;
+        public FileDealer (IFileSystem fileSystem)
+        {
+            _fileSytem = fileSystem;
+        }
+        public string FileProcessor()
+        {
+            String filePath = @"C:\Users\Mr. Erbynn\Desktop\ut\test\file.txt";
+            Console.WriteLine("file path: " + filePath);
+            String folderPath = Path.GetDirectoryName(filePath);
+            Console.WriteLine("folder path: " + folderPath);
+
+            if (!_fileSytem.Directory.Exists(folderPath))
+                _fileSytem.Directory.CreateDirectory(folderPath);
+
+            if (_fileSytem.File.Exists(filePath))
+                _fileSytem.File.Delete(filePath);
+
+            _fileSytem.File.WriteAllText(filePath, "Hello from John Erbynn");
+            var content = _fileSytem.File.ReadAllText(filePath);
+
+            Console.WriteLine(content);
+            return content;
+        }
+    }
+}
